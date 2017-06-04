6d26a4a1065c1360247fb1c5597647a2150b63fc
stas <avdosevstas@mail.ru>
2017-06-04 21:41:46 +0300

    vse norm

---
+++ saper1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace saper1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(304, 300);
+            this.panel1.TabIndex = 0;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "1.PNG");
+            this.imageList1.Images.SetKeyName(1, "22.PNG");
+            this.imageList1.Images.SetKeyName(2, "33.PNG");
+            this.imageList1.Images.SetKeyName(3, "42.PNG");
+            this.imageList1.Images.SetKeyName(4, "34.PNG");
+            this.imageList1.Images.SetKeyName(5, "35.PNG");
+            this.imageList1.Images.SetKeyName(6, "36.PNG");
+            this.imageList1.Images.SetKeyName(7, "37.PNG");
+            this.imageList1.Images.SetKeyName(8, "38.PNG");
+            this.imageList1.Images.SetKeyName(9, "39.PNG");
+            this.imageList1.Images.SetKeyName(10, "40.PNG");
+            this.imageList1.Images.SetKeyName(11, "41.PNG");
+            this.imageList1.Images.SetKeyName(12, "100.PNG");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::saper1.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(134, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 362);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ saper1/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace saper1
+{
+     
+
+    public partial class Form1 : Form
+    {
+        public int[,] osNOVNOI = new int[10,10];
+        public bool[,] flagkibool = new bool[10, 10];
+        public bool[,] okritaya = new bool[10, 10];
+        //public int[] massBomb = new int[10];
+        public List<int> massBomb = new List<int>();
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1_Click_1(null, null);
+
+
+
+            //кусок для чисел
+
+            /*for(int i=0;i<10;++i)
+            {
+                for(int a=0;a<10;++a)
+                {
+                    for(int b=0;b<8;++b)
+                    {
+                        osNOVNOI
+                    }
+
+                }
+
+            }*/
+
+
+
+
+
+        }
+
+        public bool checkBomb(int a)
+        {
+
+
+           foreach(int i in massBomb)
+            {
+                if (i == a)
+                    return true;
+            }
+            return false;
+
+
+
+        }
+
+
+        //клик правой
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            int a123 = massBomb[0];
+            //if (e.Equals(MouseButtons.Left))
+            int X = ((PictureBox)sender).Left;
+            int Y = ((PictureBox)sender).Top;
+            /* for (int i = 0; i < 10; ++i)
+                 for (int b = 0; b < 10; ++b)
+                     */
+            //if (osNOVNOI[X, Y] == -1)
+                  //  {
+                      //  bool p = false; }
+                        
+                        if (((MouseEventArgs)e).Button== MouseButtons.Left)
+                {
+
+               
+
+                if (osNOVNOI[X / 30, Y / 30] == -1)
+                {
+                    
+                    for (int b = 0; b < 10; ++b)
+                    {
+                        
+                            foreach (Control i in panel1.Controls)
+                        {
+                            PictureBox a = i as PictureBox;
+                            if (a != null)
+                            {
+                                if(massBomb[b]%10==a.Left/30&& massBomb[b] / 10 == a.Top/30)
+                                {
+                                    a.Image = imageList1.Images[12];
+                                }
+                               
+                            }
+                        }
+
+
+                    }
+
+
+
+                        MessageBox.Show("LOOSE");
+                    pictureBox1_Click_1(null, null);
+
+                }
+                else
+                {
+                    //сюда число которое будет появляться
+                    okritaya[X / 30, Y / 30] = true;
+                    ((PictureBox)sender).Image= imageList1.Images[osNOVNOI[X / 30, Y / 30 ]+3];
+                }
+            }
+            else
+            {
+                if(!okritaya[X / 30, Y / 30])
+                {
+
+                
+                //поменять картинку а не установить
+                flagkibool[X / 30, Y / 30] = flagkibool[X / 30, Y / 30] ? false : true;
+                if(flagkibool[X / 30, Y / 30])
+                ((PictureBox)sender).Image = imageList1.Images[2];
+                else
+                    ((PictureBox)sender).Image = imageList1.Images[1];
+                }
+            }
+
+        }
+
+
+
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {//новая игра
+            List<Control> massDelet = new List<Control>() ;
+            foreach(Control i in panel1.Controls)
+            {
+                PictureBox a = i as PictureBox;
+                if(a!=null)
+                {
+                    massDelet.Add(a);
+                }
+            }
+            for(int i = massDelet.Count; i>0;--i)
+            {
+                massDelet[i-1].Dispose();
+
+            }
+
+
+            
+            for (int i = 0; i < 10; ++i)
+            {
+                for (int a = 0; a < 10; ++a)
+                {
+                    PictureBox tecpic = new PictureBox();
+                    tecpic.Image = imageList1.Images[1];
+                    tecpic.Size = new Size(30, 30);
+
+                    tecpic.MouseDown += pictureBox1_Click;
+
+                    tecpic.Location = new Point(a * 30, i * 30);
+                    panel1.Controls.Add(tecpic);
+                }
+
+            }
+            for (int a = 0; a < 10; ++a)
+
+                for (int b = 0; b < 10; ++b)
+                    osNOVNOI[a, b] = 0;
+            massBomb.Clear();
+            while (massBomb.Count() != 10)
+            {
+                int bomba = rand.Next(0, 100);
+                while (checkBomb(bomba))
+                {
+                    bomba = rand.Next(0, 100);
+                }
+                massBomb.Add(bomba);
+                osNOVNOI[bomba % 10, bomba / 10] = -1;
+
+            }
+
+            
+                
+
+                    for (int i = 0; i < 10; ++i)
+            {
+                //massBomb[i]
+                for (int a = massBomb[i] % 10 - 1; a <= massBomb[i] % 10 + 1; ++a)
+                {
+                    for (int b = massBomb[i] / 10 - 1; b <= massBomb[i] / 10 + 1; ++b)
+                    {
+                        try
+                        {
+                            if (osNOVNOI[a, b] != -1)
+                                osNOVNOI[a, b] += 1;
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+
+            }
+
+
+        }
+    }
+}
+
+
+/*
+if(e.Equals(MouseButtons.Left))
+            {
+
+                int X = ((PictureBox)sender).Left;
+                int Y = ((PictureBox)sender).Top;
+
+                if(osNOVNOI[X/30+1,Y/30+1]==-1)
+                {
+                    //this.Close();
+                    // ((PictureBox)sender)
+                    MessageBox.Show("LOOSE");
+                    
+                }
+                else
+                {
+                    //сюда число которое будет появляться
+                }
+            }
+            else
+            {
+                ((PictureBox)sender).Image = imageList1.Images[2];
+            }
+
+
+    */
